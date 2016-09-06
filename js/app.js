e1daabfde2817e914bb5e2ea207f511f9d777528
agaco <agnieszka0czaplicka@gmail.com>
2016-09-06 14:23:44 +0200

    clock

--- js/app.js
+++ js/app.js
@@ -1,39 +1,26 @@
 $(document).ready(function(){
 	
-var next = $("#next"),
-    prev = $("#prev"),
-    currentIndex = 0,
-    imgNumber = $(".slider").find("div").length,
-    img = $(".slider").find("div"),
-    width = $(".slider").width();
-    
-    //console.log(imgNumber);
-    
-  
-    
-//NEXT    
-    next.on("click", function(event){
-      img.eq(currentIndex).css("display", "none") 
-      img.eq(currentIndex+1).css("display", "inline-block");
-		currentIndex++;
-        //console.log(currentIndex);
-        if (currentIndex > (imgNumber-1)) {
-            currentIndex = 0;
-			//console.log("po przeładowaniu " + currentIndex);
-        }
-			img.eq(currentIndex).css("display", "inline-block");
+//$(window).on("load", function(event){
+//	console.log("test na star");
+	
+	//$("span.js-clock").text("trwaa");
+	
+		var today = new Date();
+		var h = today.getHours();
+		var m = today.getMinutes();
+		var s = today.getSeconds();
+	
+	setInterval(function() {
+		$("span.js-clock").text(h + ":" + m + ":" + s);
 		
-  })
-//PREV   
-    prev.on("click", function(event){
-      img.eq(currentIndex).css("display", "none") 
-      currentIndex--;
-		//console.log(currentIndex);
-		if (currentIndex < 0) {
-			currentIndex = imgNumber-1;
-			//console.log(currentIndex);
-		}
-      img.eq(currentIndex).css("display", "inline-block");
-  })
+		h = today.getHours();
+		m = today.getMinutes();
+		s = today.getSeconds();
+	}, 1000);
+		
+//		
+//});
+	
 
+    
 });
